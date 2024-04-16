3e15097d4574717fef756b56a91162a338de8e78
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2024-04-16 08:59:41 -0300

    projeto inicial vazio

---
+++ src/test/java/br/com/danielvazmartins/myResume/MyResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielvazmartins.myResume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
